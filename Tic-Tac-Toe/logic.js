4af521056a0080df08c4064091fca260067a54b9
Parth Gakhar <pgakhar98@gmail.com>
2020-08-27 12:55:27 +0530

    Did some refactoring of the code

--- Tic-Tac-Toe/logic.js
+++ Tic-Tac-Toe/logic.js
@@ -1,38 +1,57 @@
 
 let activePlayer;
-let cells = document.querySelectorAll(".cell");
+let cellElements = document.querySelectorAll(".cell");
 let status = document.querySelector(".status");
+let winningMsgElement = document.querySelector(".winning-msg");
+let winningMsgContent = document.querySelector(".winning-msg-content");
+let restartButton = document.querySelector('.res');
 
-//Switching Player(O/X) after each Turn
-function switchPlayer() {
-    activePlayer = (activePlayer == 'O') ? 'X' : 'O'
-}
-//Pushing X/O into the boxes
-function pushValue() {
-    this.innerText = activePlayer;
-    let winner = checkWin();
-    if (!winner) {
-        this.removeEventListener('click', pushValue)
-        switchPlayer();
-        status.innerText = activePlayer + "'s Turn!"
+init();
+
+//Determines state of the board at the start of game
+function init() {
+    activePlayer = 'O';
+    status.innerHTML = "O's Turn!";
+    winningMsgElement.classList.remove("show");
+    status.classList.remove("notShow");
+    for (let i = 0; i < cellElements.length; i++) {
+        cellElements[i].innerText = '';
+        cellElements[i].addEventListener('click', handleClick, { once: true });
+        cellElements[i].classList.remove("clicked");
 
     }
-    else if (winner == "Tie") {
-        this.removeEventListener('click', pushValue);
-        status.innerText = "It's a Tie!";
-        for (let i = 0; i < cells.length; i++) {
-            cells[i].addEventListener('click', init);
-        }
+}
+//Switching Player(X/O) after each Turn
+function switchPlayer() {
+    activePlayer = (activePlayer == 'O') ? 'X' : 'O';
+}
+//Handling the click event on each cell
+function handleClick(e) {
+    let cell = e.target;
+    //Putting value in the cell
+    cell.innerText = activePlayer;
+
+    cell.classList.add("clicked");
+    //Checking for winner
+    let winner = checkWin(activePlayer);
+    if (winner) {
+        winningMsgContent.innerText = activePlayer + " wins the Game! "
+        winningMsgElement.classList.add("show");
+        status.classList.add("notShow");
+    }
+    else if (checkDraw()) {
+        winningMsgContent.innerText = "It's a Tie!";
+        winningMsgElement.classList.add("show");
+        status.classList.add("notShow");
     }
     else {
-        status.innerText = winner + " wins the Game! Congrats"
-        for (let i = 0; i < cells.length; i++) {
-            cells[i].addEventListener('click', init);
-        }
+        switchPlayer();
+        status.innerText = activePlayer + "'s Turn!"
     }
+
 }
 //Checking whether any player has won the game after each move
-function checkWin() {
+function checkWin(activePlayer) {
     //Winning Combos on the board
     const winningCombo = [
         [0, 1, 2],
@@ -44,40 +63,21 @@ function checkWin() {
         [0, 4, 8],
         [2, 4, 6]
     ]
-    let winner = null;
-    let cval = [];
-    for (let i = 0; i < cells.length; i++) {
-        cval[i] = cells[i].innerText;
-    }
-    for (let i = 0; i < winningCombo.length; i++) {
-        if (cval[winningCombo[i][0]] &&
-            cval[winningCombo[i][0]] == cval[winningCombo[i][1]] &&
-            cval[winningCombo[i][1]] == cval[winningCombo[i][2]]) {
-            cells[winningCombo[i][0]].classList.add("win");
-            cells[winningCombo[i][1]].classList.add("win");
-            cells[winningCombo[i][2]].classList.add("win");
-            winner = cval[winningCombo[i][0]];
-            break;
+    return winningCombo.some(combo => {
+        return combo.every(item => {
+            return cellElements[item].innerText == activePlayer;
+
         }
-    }
-    //Condition for Tie
-    if (winner == null && cval.includes("") == false) {
-        winner = "Tie";
-    }
-    return winner;
+        )
+    })
+  
+}
+function checkDraw() {
+    return [...cellElements].every(item => {
+        return item.innerText.includes("X") || item.innerText.includes("O");
+    })
 }
 
+//Event listener for restart button
+restartButton.addEventListener('click', init);
 
-document.querySelector('#res').addEventListener('click', init);
-
-function init() {
-    activePlayer = 'O';
-    for (let i = 0; i < cells.length; i++) {
-        cells[i].innerText = '';
-        cells[i].removeEventListener('click', init);
-        cells[i].addEventListener('click', pushValue);
-        cells[i].classList.remove("win");
-    }
-    status.innerHTML = "O's Turn!";
-}
-init();
